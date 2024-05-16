311101016145583fed9904bc146e83149096e27e
HenintsoaJunior <rakotoarimananahentsa@gmail.com>
2024-05-16 17:05:54 +0300

    sprint1-2802

--- Framework/jar.bat
+++ Framework/jar.bat
@@ -2,7 +2,7 @@ cd src
 set "lib=E:\jar\lib"
 javac -d . -cp "%lib%\*" *.java
 jar cvf framework.jar etu2802
-move framework.jar C:\Users\Henintsoa\Documents\github\Framework\sprint0-2802\TestFramework\lib
+move framework.jar C:\Users\Henintsoa\Documents\github\MrNainaS4\S4\Framework\TestFramework\lib
 cd ../
 
 
---
+++ Framework/src/AnnotationController.java
@@ -0,0 +1,9 @@
+package etu2802;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface AnnotationController {
+    String value(); // Attribut pour représenter le nom du contrôleur
+}
