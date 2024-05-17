bf5961c6f5026073fea675fc70634fbfa14efc3c
HenintsoaJunior <rakotoarimananahentsa@gmail.com>
2024-05-17 03:07:35 +0300

    sprint1-2802

--- Framework/src/FrontController.java
+++ Framework/src/FrontController.java
@@ -14,7 +14,7 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 
 public class FrontController extends HttpServlet {
-    private List<Class<?>> controllerClasses;
+    private List<Class<?>> controllerClasses; //liste pour stocker les classes des contrôleurs
 
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
@@ -23,24 +23,24 @@ public class FrontController extends HttpServlet {
     }
 
     public List<Class<?>> allMappingUrls(String pckg) {
-        controllerClasses = new ArrayList<>();
+        controllerClasses = new ArrayList<>(); 
 
-        ServletContext context = getServletContext();
+        ServletContext context = getServletContext(); // Obtention du contexte du servlet
         String path = "/WEB-INF/classes/" + pckg;
 
-        Set<String> classNames = context.getResourcePaths(path);
+        Set<String> classNames = context.getResourcePaths(path);// Récupération des chemins des ressources dans le package
         if (classNames != null) {
             for (String className : classNames) {
                 if (className.endsWith(".class")) {
-                    String fullClassName = className.substring(0, className.length() - 6);
+                    String fullClassName = className.substring(0, className.length() - 6); // Suppression de l'extension ".class"
                     int taille = fullClassName.split("/").length;
-                    fullClassName = fullClassName.split("/")[taille - 2] + "." + fullClassName.split("/")[taille - 1];
+                    fullClassName = fullClassName.split("/")[taille - 2] + "." + fullClassName.split("/")[taille - 1]; // Construction du nom complet de la classe 
                     try {
                         Class<?> myClass = Class.forName(fullClassName);
-                        AnnotationController annotation = myClass.getAnnotation(AnnotationController.class);
+                        AnnotationController annotation = myClass.getAnnotation(AnnotationController.class); // Récupération de l'annotation
                         if (annotation != null) {
                             System.out.println("anotation value"+annotation.value());
-                            controllerClasses.add(myClass);
+                            controllerClasses.add(myClass); // Ajout de la classe à la liste
                         }
                     } catch (Exception e) {
                         System.out.println(e);
