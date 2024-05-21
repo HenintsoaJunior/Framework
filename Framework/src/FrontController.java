ebc153d689a7b7b50363122f0dec486af6b9d8d3
HenintsoaJunior <rakotoarimananahentsa@gmail.com>
2024-05-21 14:49:18 +0300

    sprint2-2802

--- Framework/src/FrontController.java
+++ Framework/src/FrontController.java
@@ -2,12 +2,9 @@ package etu2802;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -40,67 +37,9 @@ public class FrontController extends HttpServlet {
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         String pkg = this.getInitParameter("package");
-        controllerClasses = getAllControllers(pkg);
-        mappingUrls = allMappingUrls(pkg);
-    }
-
-    public List<Class<?>> getAllControllers(String pckg) {
-        List<Class<?>> controllerClasses = new ArrayList<>();
         ServletContext context = getServletContext();
-        String path = "/WEB-INF/classes/" + pckg.replace('.', '/');
-        
-        Set<String> classNames = context.getResourcePaths(path);
-        if (classNames != null) {
-            for (String className : classNames) {
-                if (className.endsWith(".class")) {
-                    String fullClassName = className.substring("/WEB-INF/classes/".length(), className.length() - 6).replace('/', '.');
-                    try {
-                        Class<?> myClass = Class.forName(fullClassName);
-                        if (myClass.isAnnotationPresent(AnnotationController.class)) {
-                            controllerClasses.add(myClass);
-                        }
-                    } catch (ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        } else {
-            System.out.println("Class Name null");
-        }
-        return controllerClasses;
-    }
-
-    public HashMap<String, Mapping> allMappingUrls(String pckg) {
-        HashMap<String, Mapping> mappingUrl = new HashMap<>();
-        ServletContext context = getServletContext();
-        String path = "/WEB-INF/classes/" + pckg.replace('.', '/');
-
-        Set<String> classNames = context.getResourcePaths(path);
-        if (classNames != null) {
-            for (String className : classNames) {
-                if (className.endsWith(".class")) {
-                    String fullClassName = className.substring("/WEB-INF/classes/".length(), className.length() - 6).replace('/', '.');
-                    try {
-                        Class<?> myClass = Class.forName(fullClassName);
-                        for (Method method : myClass.getDeclaredMethods()) {
-                            if (method.isAnnotationPresent(Url.class)) {
-                                Url url = method.getAnnotation(Url.class);
-                                Mapping map = new Mapping(myClass.getSimpleName(), method.getName());
-                                if (mappingUrl.containsKey(url.lien())) {
-                                    throw new IllegalArgumentException("Duplicate URL mapping found: " + url.lien());
-                                }
-                                mappingUrl.put(url.lien(), map);
-                            }
-                        }
-                    } catch (ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        } else {
-            System.out.println("Class Name null");
-        }
-        return mappingUrl;
+        controllerClasses = Utils.getAllControllers(context,pkg);
+        mappingUrls = Utils.allMappingUrls(context,pkg);
     }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
@@ -114,7 +53,7 @@ public class FrontController extends HttpServlet {
                 AnnotationController annotation = controllerClass.getAnnotation(AnnotationController.class);
                 if (annotation != null) {
                     String controllerName = controllerClass.getSimpleName();
-                    out.println("Controller Found Sont: " + controllerName);
+                    out.println("Controller Found: " + controllerName);
                 } else {
                     out.println("Annotation null");
                 }
---
+++ Framework/src/Utils.java
@@ -0,0 +1,71 @@
+package etu2802;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.ServletContext;
+
+public class Utils {
+    
+
+    public static List<Class<?>> getAllControllers(ServletContext context,String pckg) {
+        List<Class<?>> controllerClasses = new ArrayList<>();
+        String path = "/WEB-INF/classes/" + pckg.replace('.', '/');
+        
+        Set<String> classNames = context.getResourcePaths(path);
+        if (classNames != null) {
+            for (String className : classNames) {
+                if (className.endsWith(".class")) {
+                    String fullClassName = className.substring("/WEB-INF/classes/".length(), className.length() - 6).replace('/', '.');
+                    try {
+                        Class<?> myClass = Class.forName(fullClassName);
+                        if (myClass.isAnnotationPresent(AnnotationController.class)) {
+                            controllerClasses.add(myClass);
+                        }
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } else {
+            System.out.println("Class Name null");
+        }
+        return controllerClasses;
+    }
+
+    public static HashMap<String, Mapping> allMappingUrls(ServletContext context,String pckg) {
+        HashMap<String, Mapping> mappingUrl = new HashMap<>();
+        String path = "/WEB-INF/classes/" + pckg.replace('.', '/');
+
+        Set<String> classNames = context.getResourcePaths(path);
+        if (classNames != null) {
+            for (String className : classNames) {
+                if (className.endsWith(".class")) {
+                    String fullClassName = className.substring("/WEB-INF/classes/".length(), className.length() - 6).replace('/', '.');
+                    try {
+                        Class<?> myClass = Class.forName(fullClassName);
+                        for (Method method : myClass.getDeclaredMethods()) {
+                            if (method.isAnnotationPresent(Url.class)) {
+                                Url url = method.getAnnotation(Url.class);
+                                Mapping map = new Mapping(myClass.getSimpleName(), method.getName());
+                                if (mappingUrl.containsKey(url.lien())) {
+                                    throw new IllegalArgumentException("Duplicate URL mapping found: " + url.lien());
+                                }
+                                mappingUrl.put(url.lien(), map);
+                            }
+                        }
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } else {
+            System.out.println("Class Name null");
+        }
+        return mappingUrl;
+    }
+
+}
