7240e4516d52c463c89c114fb668969b4f06d211
HenintsoaJunior <rakotoarimananahentsa@gmail.com>
2024-06-07 12:02:53 +0300

    spint5-2802

--- Framework/src/FrontController.java
+++ Framework/src/FrontController.java
@@ -2,13 +2,10 @@ package etu2802;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.List;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -39,58 +36,27 @@ public class FrontController extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        String pkg = this.getInitParameter("package");
-        ServletContext context = getServletContext();
-        controllerClasses = Utils.getAllControllers(context, pkg);
-        mappingUrls = Utils.allMappingUrls(context, pkg);
-    }
-
-    public Method getMethodByUrl(Object objet, String mappingUrlkey) throws Exception {
-        Method[] all_methods = objet.getClass().getDeclaredMethods();
-        for (int i = 0; i < all_methods.length; i++) {
-            Annotation[] annotations = all_methods[i].getAnnotations();
-            for (int j = 0; j < annotations.length; j++) {
-                if (annotations[j].annotationType() == Url.class) {
-                    Url url = (Url) annotations[j];
-                    if (url.lien().compareTo(mappingUrlkey) == 0 && all_methods[i].getName().compareTo(mappingUrls.get(mappingUrlkey).getMethod()) == 0) {
-                        return all_methods[i];
-                    }
-                }
-            }
-        }
-        throw new Exception("Method not found");
-    }
-
-    public void dispatchModelView(HttpServletRequest request, HttpServletResponse response, Object object, String mappingUrlKey) throws Exception {
-        PrintWriter out = response.getWriter();
-        Method method = this.getMethodByUrl(object, mappingUrlKey);
-        Object returnValue = method.invoke(object);
-
-        if (returnValue instanceof String) {
-            out.println("La valeur retournée : " + returnValue);
-        } else if (returnValue instanceof ModelView) {
-            ModelView mv = (ModelView) returnValue;
-            for (String mvKey : mv.getData().keySet()) {
-                request.setAttribute(mvKey, mv.getData().get(mvKey));
-            }
-
-            RequestDispatcher dispatcher = request.getRequestDispatcher(mv.getView());
-            dispatcher.forward(request, response);
-        } else {
-            throw new Exception("Unsupported return type");
+        try {
+            String pkg = this.getInitParameter("package");
+            ServletContext context = getServletContext();
+            controllerClasses = Utils.getAllControllers(context, pkg);
+            mappingUrls = Utils.allMappingUrls(context, pkg);   
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+    
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/plain");
+        response.setContentType("text/html");
         try (PrintWriter out = response.getWriter()) {
             String url = request.getRequestURI();
             String contextPath = request.getContextPath();
             url = url.substring(contextPath.length());
-
-            Mapping map = mappingUrls.get(url);
-
-            if (map != null) {
+    
+            try {
+                Mapping map = mappingUrls.get(url);
+    
                 out.println("URL: " + url + " -> Controller: " + map.getClassName() + ", Method: " + map.getMethod());
                 try {
                     String packageName = this.getInitParameter("package");
@@ -98,21 +64,20 @@ public class FrontController extends HttpServlet {
                     Class<?> clazz = Class.forName(fullClassName);
                     Object object = clazz.newInstance();
                     
-                    dispatchModelView(request, response, object, url);
-
+                    Utils.dispatchModelView(request, response, object, url, mappingUrls);
+    
                 } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
                     out.println("Erreur lors de l'invocation de la méthode : " + e.getMessage());
                     e.printStackTrace();
-                } catch (Exception e) {
-                    out.println("Erreur : " + e.getMessage());
-                    e.printStackTrace();
                 }
-            } else {
-                out.println("URL not found");
+            } catch (Exception e) {
+                Utils.generateNotFoundPage(out);
+                e.printStackTrace();
             }
         }
     }
-
+    
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
