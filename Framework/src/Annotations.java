33cb22d8e9fe51d52e11e1d0e511715b1e0b6135
Daxido junior <143500070+HenintsoaJunior@users.noreply.github.com>
2024-06-18 17:36:05 +0300

    Sprint7 2802

---
+++ Framework/src/Annotations.java
@@ -0,0 +1,26 @@
+package etu2802;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public class Annotations {
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface AnnotationController {
+        String value();
+    }
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface AnnotationAttribute {
+        String value();
+    }
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface AnnotationMethod {
+        String value(); 
+    }
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.PARAMETER)
+    public @interface AnnotationParameter {
+        String value(); 
+    }
+}
